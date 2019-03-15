cfef4b3cfe274d09c67807d45bd9cf8b981a828e
Sebastian Basner <sebastian.basner@gmx.de>
2019-03-15 21:43:47 +0100

    Initial project setup

---
+++ src/main/java/de/fnortheim/ayrpetclinic/AyrPetClinicApplication.java
@@ -0,0 +1,13 @@
+package de.fnortheim.ayrpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyrPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AyrPetClinicApplication.class, args);
+    }
+
+}
