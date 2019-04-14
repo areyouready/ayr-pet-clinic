b5b791beabfc9618cd1a5f898752d77f0bb44d17
Sebastian Basner <open.sebastian@gmail.com>
2019-04-14 22:31:31 +0200

    Added Vets to the model returned by VetController. Added Vetlist to the view. Closes #15

--- pet-clinic-web/src/main/java/de/fnortheim/ayrpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/de/fnortheim/ayrpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package de.fnortheim.ayrpetclinic.controllers;
 
+import de.fnortheim.ayrpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -9,8 +11,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
