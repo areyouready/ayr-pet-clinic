40624d677964901631adfd30f645b3a2843b090d
Sebastian Basner <open.sebastian@gmail.com>
2019-05-03 19:13:34 +0200

    Added pet relation to Owner entitiy; Created Visit Entity.
    Closes #26

--- pet-clinic-data/src/main/java/de/fnortheim/ayrpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/de/fnortheim/ayrpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package de.fnortheim.ayrpetclinic.model;
 
+import java.util.Set;
+
 /**
  * created by sebastian on Apr, 2019
  */
 public class Owner extends Person {
+
+    private Set<Pet> petSet;
+
+    public Set<Pet> getPetSet() {
+        return petSet;
+    }
+
+    public void setPetSet(Set<Pet> petSet) {
+        this.petSet = petSet;
+    }
 }
