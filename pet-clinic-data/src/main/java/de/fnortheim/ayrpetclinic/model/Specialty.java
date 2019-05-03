c9d504a315028bf036a1a9319fbd351d09058116
Sebastian Basner <open.sebastian@gmail.com>
2019-05-03 19:18:27 +0200

    Created specialties and added them to vet. Closes #27

---
+++ pet-clinic-data/src/main/java/de/fnortheim/ayrpetclinic/model/Specialty.java
@@ -0,0 +1,17 @@
+package de.fnortheim.ayrpetclinic.model;
+
+/**
+ * created by sebastian on May, 2019
+ */
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/de/fnortheim/ayrpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/de/fnortheim/ayrpetclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package de.fnortheim.ayrpetclinic.model;
 
+import java.util.Set;
+
 /**
  * created by sebastian on Apr, 2019
  */
 public class Vet extends Person {
+
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
