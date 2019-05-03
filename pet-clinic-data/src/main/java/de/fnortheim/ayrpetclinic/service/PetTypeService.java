7101a65bea749c259420367783ea2c6c1a604136
Sebastian Basner <open.sebastian@gmail.com>
2019-05-03 22:41:28 +0200

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/de/fnortheim/ayrpetclinic/service/PetTypeService.java
@@ -0,0 +1,9 @@
+package de.fnortheim.ayrpetclinic.service;
+
+import de.fnortheim.ayrpetclinic.model.PetType;
+
+/**
+ * created by sebastian on May, 2019
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/de/fnortheim/ayrpetclinic/service/map/PetTypeServiceMap.java
@@ -0,0 +1,38 @@
+package de.fnortheim.ayrpetclinic.service.map;
+
+import de.fnortheim.ayrpetclinic.model.PetType;
+import de.fnortheim.ayrpetclinic.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * created by sebastian on May, 2019
+ */
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
