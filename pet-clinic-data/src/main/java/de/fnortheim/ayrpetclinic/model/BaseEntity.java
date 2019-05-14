50496b09cf42b5272a44bc82c9f15d2811bc839a
Sebastian Basner <open.sebastian@gmail.com>
2019-05-14 19:31:32 +0200

    Implemented BaseEntity with JPA. Closes #35

--- pet-clinic-data/src/main/java/de/fnortheim/ayrpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/de/fnortheim/ayrpetclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package de.fnortheim.ayrpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * created by sebastian on Apr, 2019
  */
+@MappedSuperclass // jpa base class where others inherit from. Therefore this class is not mapped to the db
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
